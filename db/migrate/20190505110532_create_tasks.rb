9974e3895db0e7f0a6a7e0adb3d2255053ed78e0
thr3a <12jk172@ms.dendai.ac.jp>
2019-05-05 20:07:04 +0900

    update: DBのデフォルト値設定

--- db/migrate/20190505110532_create_tasks.rb
+++ db/migrate/20190505110532_create_tasks.rb
@@ -1,8 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.string :name
-      t.boolean :is_done
+      t.string :name, null: false
+      t.boolean :is_done, default: false, null: false
 
       t.timestamps
     end
